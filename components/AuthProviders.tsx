16e32bd53e636dd8c76e640f5f2dd033ba909d2e
nghidaide012 <nghitrn135@yahoo.com>
2023-08-24 08:14:14 +0200

    user can upload, edit, delete  and view other user project

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from "react"
+import { getProviders, signIn } from "next-auth/react";
+import Button from "./Button";
+
+type Provider = {
+    id:string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>
+
+const AuthProviders = () => {
+
+    const [providers, setProviders] = useState<Providers | null>(null); 
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+            console.log(res);
+            setProviders(res);
+        }
+        fetchProviders();
+    }, [])
+
+    if(providers)
+    {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, index) => (
+                    <Button 
+                        key={index}
+                        handleClick={() => signIn(provider?.id)}
+                        title="Sign In"
+                        />
+                    
+                ))}
+            </div>
+        )
+    }
+}
+
+export default AuthProviders
---
+++ components/ProjectActions.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, {useState} from 'react'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { deleteProject, fetchToken } from '@/lib/actions'
+import { useRouter } from 'next/navigation'
+
+const ProjectActions = ({projectId}: {projectId: string}) => {
+    const router = useRouter();
+    const [isDeleting, setisDeleting] = useState(false)
+    const handleDeleteProject = async () => {
+        setisDeleting(true);
+        const {token}  = await fetchToken();
+        try {
+            await deleteProject(projectId, token);
+            router.push('/');
+        } catch (error) {
+            console.log(error);
+            
+        }
+        finally {
+            setisDeleting(false);
+        }
+    }
+
+  return (
+    <>
+    <Link
+        href={`/edit-project/${projectId}`}
+        className='flexCenter edit-action_btn'
+        >
+            <Image src="/pencile.svg" width={15} height={15} alt='Edit' />
+    </Link>
+    <button type='button' className={`flexCenter delete-action_btn ${isDeleting ?'bg-gray' : 'bg-primary-purple'}`}
+            onClick={handleDeleteProject}
+    >
+            <Image src="/trash.svg" width={15} height={15} alt='delete' />     
+    </button>
+    </>
+  )
+}
+
+export default ProjectActions
